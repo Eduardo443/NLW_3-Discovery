16008b5c3b6c02cccfdb1ec4d72afa6885108b0a
Eduardo Hensel Batista <57881348+Eduardo443@users.noreply.github.com>
2020-10-17 18:48:46 -0300

    Add files via upload

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+
+    {
+        id: 1,
+        lat: "-26.8807883",
+        lng: "-49.0885652",
+        name: "Lar das meninas",
+        description: "Pestando assistencia a crianças de 06 a 15 anos que se encontren em situações de risco e/ou vulnerabilidade",
+        images: [
+            "https://images.unsplash.com/photo-1598539961915-040bb3be3f69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: '1'
+
+    },
+    {
+        id: 2,
+        lat: "-26.8907883",
+        lng: "-49.0985652",
+        name: "Lar dos meninos",
+        description: "Pestando assistencia a crianças de 06 a 15 anos que se encontren em situações de risco e/ou vulnerabilidade",
+        images: [
+            "https://images.unsplash.com/photo-1598539961915-040bb3be3f69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: '0'
+    }
+
+]
